019162dc5a7d49d8056481a7c3bc300e86faee41
ayseakyol <ayseakyol.22309@gmail.com>
2020-03-25 17:20:02 +0100

    version3

---
+++ version3.js
@@ -0,0 +1,18 @@
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log("My todos", this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
