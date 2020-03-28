23252e587842bab691f79010259af04532ee9be3
ayseakyol <ayseakyol.22309@gmail.com>
2020-03-28 16:58:08 +0100

    todo folder

---
+++ todo/listener.js
@@ -0,0 +1,6 @@
+document.getElementById("toggleAllButton").addEventListener("click", toggleAll);
+document.getElementById("addButton").addEventListener("click", addTodo);
+document.getElementById("changeButton").addEventListener("click", changeTodo);
+document
+  .getElementById("toggleCompletedButton")
+  .addEventListener("click", toggleCompleted);
